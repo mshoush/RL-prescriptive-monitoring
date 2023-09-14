35c252f3d1cea08cd9551ec6a5157b7dc562bfd9
Mahmoud shoush <mahmoud.kamel104@gmail.com>
2023-09-14 10:36:01 +0300

    Add script to run the offline phase

--- run_offline_phase.sh
+++ run_offline_phase.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # List of datasets to process
-datasets=("trafficFines" "bpic2012" "bpic2017")
+datasets=("trafficFines" "bpic2017" "bpic2012")
 
 # Loop through each dataset
 for dataset in "${datasets[@]}"
 do
     echo "Starting predictive model for $dataset...";
-    # change the size of the ensemble as your needs
-    ens_size=20;
-    python predictive_model/get_catboost_pred_uncer.py $dataset results/predictive/$dataset/ $ens_size > out.txt;
+    # change the size of the ensemble as you need
+    ens_size=1;
+    python -W ignore  predictive_model/get_catboost_pred_uncer.py $dataset results/predictive/$dataset/ $ens_size > out.txt;
     rm out.txt
     sleep 5;
 
     echo "Starting causal analysis for $dataset...";
-    python causal/causallift_adapted.py $dataset ./results/causal/$dataset/ ./results/predictive/$dataset/ > out.txt;
+    python -W ignore causal/causallift_adapted.py $dataset ./results/causal/$dataset/ ./results/predictive/$dataset/ > out.txt;
     rm out.txt
     sleep 5;
 
     echo "Running lower and upper bounds CATE analysis for $dataset (Zahra)...";
-    python causal/lower_upper_cate.py --data=$dataset --results_dir=./results/causal/$dataset/ > out.txt;
+    python -W ignore causal/lower_upper_cate.py --data=$dataset --results_dir=./results/causal/$dataset/ > out.txt;
     rm out.txt
     sleep 5;
 
@@ -29,17 +29,17 @@ do
     sleep 5
 
     echo "Starting conformal prediction for $dataset...";
-    python conformal_prediction/conformal_prediction.py $dataset ./results/predictive/$dataset/ ./results/conformal/$dataset/ ./results/causal/$dataset/ > out.txt;
+    python -W ignore conformal_prediction/conformal_prediction.py $dataset ./results/predictive/$dataset/ ./results/conformal/$dataset/ ./results/causal/$dataset/ > out.txt;
     rm out.txt
     sleep 5;
 
     echo "Running conformalized survival model for $dataset...";
-    python conformalized_survival_model_final_v1.py $dataset > out.txt;
+    python -W ignore conformalized_survival_model_final_v1.py $dataset > out.txt;
     rm out.txt
     sleep 5
 
     echo "Preparing data for Reinforcement Learning for $dataset..."
-    python prepare_data_for_RL_V2.py $dataset > out.txt;
+    python -W ignore prepare_data_for_RL_V2.py $dataset > out.txt;
     rm out.txt
     
     echo "Processing completed for $dataset.";
